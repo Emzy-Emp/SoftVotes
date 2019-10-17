bae0659aff9d9bfd2b10bb1ce6c8cfe3858f4787
Emmanuel <Emmanuel@Emzy_Empire>
2019-10-17 17:18:35 +0100

    initial check-in of the SoftVotes

---
+++ SoftVotes/src/main/java/com/softvotes/SoftVotesApplication.java
@@ -0,0 +1,13 @@
+package com.softvotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftVotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftVotesApplication.class, args);
+	}
+
+}
---
+++ SoftVotes/src/test/java/com/softvotes/SoftVotesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.softvotes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SoftVotesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
